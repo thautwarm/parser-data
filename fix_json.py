1229582093d07107e60787ba1a442da2a40354de
thautwarm <twshere@outlook.com>
2022-01-05 01:57:14 +0900

    smaller

--- fix_json.py
+++ fix_json.py
@@ -2,6 +2,7 @@ import os
 import io
 xs = open("test.json", 'w', encoding='utf-8')
 xs.write('[')
+cnt = 0
 
 trailing = ""
 with open("european-20181028.json", encoding='utf-8-sig') as f:
@@ -10,6 +11,9 @@ with open("european-20181028.json", encoding='utf-8-sig') as f:
         xs.write(line)
         if line.strip() == '}':
             trailing = ","
+            cnt += 1
+            if cnt > 500:
+                break
         else:
             trailing = ""
 xs.write("]")
